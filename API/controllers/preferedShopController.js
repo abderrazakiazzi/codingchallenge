cc05a3e3d2595beaf8c22d95d84c4215caf58e1e
abderrazak iazzi <abdou.iazzi@gmail.com>
2019-06-07 10:25:27 +0000

    get prefered shops function update 1

--- API/controllers/preferedShopController.js
+++ API/controllers/preferedShopController.js
@@ -1,27 +1,72 @@
 var mongoose = require('mongoose');
 var Shops = mongoose.model('shops');
-var PreferedShops = mongoose.model('preferedshops');
+var PreferedShops = mongoose.model('Preferedshops');
+//var assert = require('assert');
 
 // get all shops
 exports.listPreferedShops = function (req, res) {
-    return PreferedShops.find({}, (err, shops) => {
-        if (err) {
-            console.log('No element found !!');
-            res.send(err);
-            //return;
-        }
-        if (!shops) {
-            console.log('no element founf');
-            res.json(new Shops());
-            //return;
+    console.log('test list shop ');
+    var array_pShop = [];
+    var pShop1 = PreferedShops.find({}).exec();
+    console.log(' resultat size = ' + Array.from(pShop1).length);
+    Shops.aggregate([{
+        $lookup:
+        {
+            from: PreferedShops,
+            localField: '_id',
+            foreignField: 'id_shop',
+            as : 'preferedShop'
+        } 
+    }], shoparray =>{
+        let _array = shoparray;
+        if(!Array.isArray(_array)){
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+            return  res.json(_array);
         }
         else {
-            console.log('shops were found');
-            res.json(shops);
-            // return;
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+            return res.json({'result': 'empty list'});
         }
-        //res.end();
     });
+
+    /*.toArray(function(err, res){
+        if(err){
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+            res.json({'result ': 'error'});
+        }else{
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+            res.json(res);
+        }
+    });
+   /* return PreferedShops.find({}, function(err, shop){
+        //var array_shop = [];
+        if(err){
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+            res.json({'result ': 'error '});
+        }
+        Array.from(shop).forEach(function(err, item){
+
+            console.log(' item ==== ' + shop[item].id_shop);
+          Shops.findById(shop[item].id_shop, function(err, listshop){
+                if(err){
+                    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+                    console.log({'result': 'error :::: no prefered shops found !!'});
+                    res.json({'result': 'error '});
+                   
+                }
+                if(!listshop){
+                    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+                    res.json({result:'empty list'});
+                }else{
+                 //array_pShop.push(listshop);
+                 res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+                 res.json(listshop);
+                }
+             });
+
+        });
+          
+    }); */
 };
 
 exports.updatePreferedShop = function (req, res) {
@@ -47,7 +92,7 @@ exports.updatePreferedShop = function (req, res) {
 
 
 exports.dislikePreferedShop = function (req, res) {
-    PreferedShops.remove({ _id: req.params.shopId }, function (err, task) {
+    PreferedShops.deleteOne({ _id: req.params.shopId }, function (err, task) {
         if (err)
             res.send(err);
         else
